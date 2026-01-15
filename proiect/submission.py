41f6408f4893568825843c86a6033a55109abdc6
KebabRonin <alrx.brss@gmail.com>
2026-01-16 01:15:42 +0200

    task 2 finish

--- proiect/submission.py
+++ proiect/submission.py
@@ -1,30 +1,44 @@
-import pandas as pd, regex as re, numpy as np
-def postprocess(dataframe, stats):
-	stats['dedup'] = 0
-	for index, row in dataframe.iterrows():
-		# remove duplicate quads
-		seen = set()
-		unique_quads = []
-		for quad in row['Quadruplet']:
-			quad_tuple = (quad['Aspect'], quad['Opinion'], quad['Category'])
-			if quad_tuple not in seen:
-				seen.add(quad_tuple)
-				unique_quads.append(quad)
-		row['Quadruplet'] = unique_quads
-		stats['dedup'] += len(row['Quadruplet']) - len(unique_quads)
-		if len(row['Quadruplet']) - len(unique_quads) != 0:
-			print("Deduplicated quads for ID:", row['ID'])
-	return dataframe
+import pandas as pd, json
+import os, regex as re
+if not os.path.exists('./subtask_1'):
+	os.mkdir('./subtask_1')
+if not os.path.exists('./subtask_2'):
+	os.mkdir('./subtask_2')
+if not os.path.exists('./subtask_3'):
+	os.mkdir('./subtask_3')
+D = 1
+#IN_NAME = './test_pred_small_25_context_va_final.jsonl'
+IN_NAME = './test_pred_t5_context_va_final.jsonl'
+DSET = 'restaurant' if D == 1 else 'laptop'
+DSET_PRE = 'rest' if D == 1 else 'lap'
+PREFIXES = [f'{DSET_PRE}26_aspect_va_dev_', f'{DSET_PRE}26_aste_dev_', f'{DSET_PRE}26_asqp_dev_']
+data = pd.read_json(IN_NAME, lines=True)
+print(data.shape)
+
+for record in data.to_dict(orient='records'):
+	for q in record['Quadruplet']:
+		del q['Opinion']
+		del q['Category']
+data.columns = ['ID', 'Aspect_VA']
+with open(f"./subtask_1/pred_eng_{DSET}.jsonl", 'w') as f:
+	for item in data.to_dict(orient='records'):
+		if re.match(f'{PREFIXES[0]}.*', item['ID']):
+			f.write(json.dumps(item) + "\n")
+
+data = pd.read_json(IN_NAME, lines=True)
+
+for record in data.to_dict(orient='records'):
+	for q in record['Quadruplet']:
+		del q['Category']
+data.columns = ['ID', 'Triplet']
+with open(f"./subtask_2/pred_eng_{DSET}.jsonl", 'w') as f:
+	for item in data.to_dict(orient='records'):
+		if re.match(f'{PREFIXES[1]}.*', item['ID']):
+			f.write(json.dumps(item) + "\n")
 
 
-js = pd.read_json('subtask_3/pred_eng_laptop.jsonl', lines=True)
-state = {'aspect_case': 0, 'opinion_case': 0, 'opinion_negation': 0}
-js = postprocess(js, state)
-#Count duplicate ids
-id_counts = js['ID'].value_counts()
-duplicate_id_count = (id_counts > 1).sum()
-print(f"Number of duplicate IDs: {duplicate_id_count}")
-
-print(state)
-with open('test_pred_postprocessed.jsonl', 'w') as f:
-	f.write(js.to_json(orient='records', lines=True))
+data = pd.read_json(IN_NAME, lines=True)
+with open(f"./subtask_3/pred_eng_{DSET}.jsonl", 'w') as f:
+	for item in data.to_dict(orient='records'):
+		if re.match(f'{PREFIXES[2]}.*', item['ID']):
+			f.write(json.dumps(item) + "\n")
